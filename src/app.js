87e1de7a458bf9377abdbf39e98ec423eeb09821
Vinicius Kiss <vinicius.kiss@xpi.com.br>
2020-08-10 14:37:52 -0300

    feat: recreate current production code using webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,95 @@
-// import component from "./component";
-// import './style.css';
+import './style.scss';
 
-// document.body.appendChild(component());
-console.log("fire");
+import logoOneDrive from './assets/onedrive.svg';
+import logoPicPay from './assets/picpay.svg';
+import logoUmbler from './assets/umbler.svg';
+import logoXP from './assets/xp.svg';
+
+function init() {
+
+  const promoLoop = createPromoLoop();
+
+  createPromoBox(promoLoop[(Math.floor(Math.random() * promoLoop.length))]);
+}
+
+function createPromoLoop() {
+  const promos = [
+    {
+      hat: "AD",
+      title: "Picpay",
+      href: "http://www.picpay.com/convite?@1UXF",
+      target: "_blank",
+      img: logoPicPay,
+      text: "Use o PicPay para pagar amigos, boletos, recarregar o celular e muito mais. Ao criar sua conta, insira o código 1UXF e ",
+      callout: "ganhe R$10 de volta.",
+    },
+    {
+      hat: "AD",
+      title: "Umbler",
+      matter: 3,
+      href: "https://www.umbler.com/br/seja-bem-vindo?a=7kly6v4e",
+      target: "_blank",
+      img: logoUmbler,
+      imgSize: 50,
+      text: "A Umbler é uma empresa de Cloud Hosting que tem a missão de facilitar a vida para agências e devs.",
+      callout: "Crie sua conta e ganhe 7 dias grátis."
+    },
+    {
+      hat: "AD",
+      title: "XP Investimentos",
+      href: "https://cadastro.xpi.com.br/desktop/step/1?utm_source=linkedin&utm_medium=colaboradores&utm_campaign=aquisicao_xp_gxp_202005_lwer_abrirconta_trafego_abrir-conta_nac&utm_content=aberta_no_nac_texto_na_na&utm_term=U00967_na_colaborador_home_na",
+      target: "_blank",
+      img: logoXP,
+      text: "As melhores opções de investimentos do mercado. Abertura de conta sem custo. Na XP nenhum cliente é igual ao outro.",
+      callout: "Abra sua conta"
+    },
+    {
+      hat: "AD",
+      title: "One Drive",
+      href: "https://onedrive.live.com?invref=5a5cb17e942df5f2&invscr=90",
+      target: "_blank",
+      img: logoOneDrive,
+      text: "Salve seus arquivos e fotos no OneDrive e acesse em qualquer dispositivo, de praticamente qualquer lugar.",
+      callout: "Crie sua conta e ganhe 0.5gb de armazenamento extra."
+    }
+  ]
+
+  let returnLoop = [];
+
+  for (let i = 0; i < promos.length; i++) {
+    const numberToRepeat = (promos[i].matter !== undefined ? promos[i].matter : 1);
+
+    for (let o = 0; o < numberToRepeat; o++) {
+      returnLoop.push(promos[i]);
+    }
+  }
+
+  return returnLoop;
+}
+
+function createPromoBox(promo) {
+  const promoBoxEl = document.getElementById("promo-box");
+
+  promoBoxEl.setAttribute("href", promo.href);
+  promoBoxEl.setAttribute("target", promo.target);
+  promoBoxEl.className = "promo-box";
+
+  promoBoxEl.innerHTML = `
+    <header class="promo-box-header">${promo.hat}</header>
+    <div class="promo-box-body">
+      <div ${(promo.imgSize ? `style="min-width: calc(${promo.imgSize}px + 1rem)"` : "")} class="promo-box-media">
+        <img ${(promo.imgSize ? `style="max-width: ${promo.imgSize}px"` : "")} src="${promo.img}" />
+      </div>
+      <div class="promo-box-ad">
+        <p>${promo.text}</p>
+      </div>
+    </div>
+  `
+
+  if (promo.imgSize !== undefined) {
+    mediaImg.style.maxWidth = promo.imgSize + "px";
+    mediaEl.setAttribute("style", "min-width: calc(" + promo.imgSize + "px + 1rem)");
+  }
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -21,22 +21,69 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader'
           },
         ],
-      },
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack Output",
+      title: "Vinicius Kiss - Desenvolvedor Frontend",
+      templateParameters: {
+        email: "contato@vkiss.com.br",
+        telefone: "(11) 985 750 139",
+        telefoneLink: "whatsapp://send?phone=5511985750139",
+        footerLinks: [
+          {
+            label: "linkedin",
+            href: "https://www.linkedin.com/in/vkiss/",
+            target: "_blank",
+          },
+          {
+            label: "github",
+            href: "https://github.com/vkiss",
+            target: "_blank",
+          },
+          {
+            label: "stackoverflow",
+            href: "https://stackoverflow.com/story/vinik",
+            target: "_blank",
+          },
+          {
+            label: "picpay",
+            href: "https://app.picpay.com/user/vkiss",
+            target: "_blank",
+          }
+        ],
+        footerNotes: [
+          "font: <a target=\"_blank\" href=\"https://sourcefoundry.org/hack/\">hack</a>",
+          "html syntax style based on <a target=\"_blank\" href=\"https://github.com/Binaryify/OneDark-Pro\">one dark pro</a>'s color palette",
+          "hosted by <a target=\"_blank\" href=\"https://www.umbler.com/br/seja-bem-vindo?a=7kly6v4e\">umbler</a>",
+          "2011+, released under the <a target=\"_blank\" href=\"https://mit-license.org/\">mit license</a>.",
+          "<a target=\"_blank\" href=\"https://github.com/vkiss/root\">source code</a>",
+        ]
+      },
+      meta: {
+        viewport: 'width=device-width,minimum-scale=1',
+        description: 'Escrevo códigos para deixar sites bonitos.',
+        robots: 'index, follow'
+      },
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
     }),
     new CleanWebpackPlugin()
   ],
