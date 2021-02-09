3386a84f7006f8310c339daaee34aa367fedc4d4
Vinicius Kiss <vinicius.kiss@xpi.com.br>
2021-02-09 17:42:43 -0300

    change: footer notes translated to pt-br

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <img src="rootFiles/favicon.png" width="256px" />
 
 ```
-app size: 55.055 kb (-0.002 kb)
+app size: 55.055 kb (=)
 ```
--- src/controllers/footer-notes.js
+++ src/controllers/footer-notes.js
@@ -1,7 +1,7 @@
 import { convertBlankSpaceToTrailingSpacesElement } from "../utils";
 
 const regularNotes = [
-  "font:#<a target=\"_blank\" href=\"https://sourcefoundry.org/hack/\">hack</a>"
+  "tipografia:#<a target=\"_blank\" href=\"https://sourcefoundry.org/hack/\">hack</a>"
 ];
 
 const createFooterNoteItem = ( content ) => {
@@ -19,23 +19,23 @@ export function createFooterNotes ( selectedColorPallete ) {
   // html syntax
   if ( selectedColorPallete.name ) {
     const palleteItemP = document.createElement( "P" );
-    palleteItemP.innerHTML = `${convertBlankSpaceToTrailingSpacesElement( ".html syntax style based on " )}${( selectedColorPallete.link ? `<a target=\"_blank\" href="${selectedColorPallete.link}">` : "" )}${convertBlankSpaceToTrailingSpacesElement( selectedColorPallete.name )}${( selectedColorPallete.link ? "</a>" : "" )}${convertBlankSpaceToTrailingSpacesElement( "'s color palette" )}`;
+    palleteItemP.innerHTML = `${convertBlankSpaceToTrailingSpacesElement( ".cor da sintaxe de html baseada no tema " )}${( selectedColorPallete.link ? `<a target=\"_blank\" href="${selectedColorPallete.link}">` : "" )}${convertBlankSpaceToTrailingSpacesElement( selectedColorPallete.name )}${( selectedColorPallete.link ? "</a>" : "" )}`;
 
     document.getElementById( "footer-notes" ).appendChild( palleteItemP );
   }
 
   // icons
   document.getElementById( "footer-notes" ).appendChild(
-    createFooterNoteItem( "icons:#<a href=\"https://www.flaticon.com/br/autores/freepik\" target=\"_blank\">freepik</a>,#<a href=\"http://vaadin.com/font-icons\" target=\"_blank\">vaadin</a>#and#<a href=\"https://commons.wikimedia.org/wiki/File:Npm-logo.svg\" target=\"_blank\">wikipedia</a>" )
+    createFooterNoteItem( "ícones:#<a href=\"https://www.flaticon.com/br/autores/freepik\" target=\"_blank\">freepik</a>,#<a href=\"http://vaadin.com/font-icons\" target=\"_blank\">vaadin</a>#and#<a href=\"https://commons.wikimedia.org/wiki/File:Npm-logo.svg\" target=\"_blank\">wikipedia</a>" )
   );
 
   // hosted by
   document.getElementById( "footer-notes" ).appendChild(
-    createFooterNoteItem( `hosted#by#<a target=\"_blank\" href=\"${UMBLERREF}\">umbler</a>` )
+    createFooterNoteItem( `hospedado#pela#<a target=\"_blank\" href=\"${UMBLERREF}\">umbler</a>` )
   );
 
   // version and source code
   document.getElementById( "footer-notes" ).appendChild(
-    createFooterNoteItem( `v${VERSION}#|#<a target="_blank" href="https://github.com/vkiss/root">source#code</a>` )
+    createFooterNoteItem( `v${VERSION}#|#<a target="_blank" href="https://github.com/vkiss/root">código#fonte</a>` )
   );
 }
